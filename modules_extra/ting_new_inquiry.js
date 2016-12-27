f4b2b698ddff5ceee1204178354337183ef63da5
Henry <cryptix@riseup.net>
2016-12-27 23:29:35 +0100

    sort skills

--- modules_extra/ting_new_inquiry.js
+++ modules_extra/ting_new_inquiry.js
@@ -1,5 +1,6 @@
 var h = require('hyperscript')
 var pull = require('pull-stream')
+var pullSort = require('pull-sort')
 
 exports.needs = {
   message_compose: 'first',
@@ -34,6 +35,13 @@ exports.create = function (api) {
       pull(
         api.ting_allskills(),
         pull.filter(function(obj) { return obj.value.content.name != "" }),
+        pullSort(function(a, b){
+          var a = a.value.content.name
+          var b = b.value.content.name
+          if(a < b) return -1;
+          if(a > b) return 1;
+          return 0;
+        }),
         pull.drain(function(skMsg) {
           skillFields.appendChild(
             h('div.skill_object.float',
@@ -45,34 +53,34 @@ exports.create = function (api) {
       )
 
       var div = h('div.scroller', h('div.scroller__wrapper',
-         h('div.profile_headline', 'describe your project'),
-        h('div.compose_wrapper',                            
-        api.message_compose(
-          {type: 'ting-inquiry'},
-          {
-            placeholder: "write a description",
-            prepublish: function (value) {
-              // get hat or no-hat
-              value.hat = null
-              var els = div.querySelectorAll('input[name=hatOrNoHat]')
-              for(var n = els.length-1; n>=0; n--) {
-                if (els[n].checked == true && els[n].value == "proj") {
-                  value.hat = require('../keys').id
+        h('div.profile_headline', 'describe your project'),
+        h('div.compose_wrapper',
+          api.message_compose(
+            {type: 'ting-inquiry'},
+            {
+              placeholder: "write a description",
+              prepublish: function (value) {
+                // get hat or no-hat
+                value.hat = null
+                var els = div.querySelectorAll('input[name=hatOrNoHat]')
+                for(var n = els.length-1; n>=0; n--) {
+                  if (els[n].checked == true && els[n].value == "proj") {
+                    value.hat = require('../keys').id
+                  }
                 }
-              }
 
-              // selected skills
-              value.skills = []
-              var els = div.querySelectorAll('input[name=wantedSkills]')
-              for(var n = els.length-1; n>=0; n--) {
-                if (els[n].checked == true) {
-                  value.skills.push(els[n].value)
+                // selected skills
+                value.skills = []
+                var els = div.querySelectorAll('input[name=wantedSkills]')
+                for(var n = els.length-1; n>=0; n--) {
+                  if (els[n].checked == true) {
+                    value.skills.push(els[n].value)
+                  }
                 }
+                return value
               }
-              return value
             }
-          }
-        )),
+          )),
 
         h('div.profile_headline_short.float.clear', 'do you want the hat?'),
         h('label.float.clear',
@@ -85,7 +93,7 @@ exports.create = function (api) {
         h('div.profile_headline_short.float.clear', 'select skill positions'),
         skillFields
 
-       
+
 
       ))
       return div
